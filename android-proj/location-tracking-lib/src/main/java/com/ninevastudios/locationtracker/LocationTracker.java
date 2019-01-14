590c55ff4646bd185367fe7212261b14ba8a552e
Taras Leskiv <tleskiv@transfollow.org>
2019-01-14 10:21:24 +0200

    initial commit

---
+++ android-proj/location-tracking-lib/src/main/java/com/ninevastudios/locationtracker/LocationTracker.java
@@ -0,0 +1,7 @@
+package com.ninevastudios.locationtracker;
+
+import android.support.annotation.Keep;
+
+@Keep
+public class LocationTracker {
+}
---
+++ android-proj/myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ninevastudios.locationtracker.GoogleMapsManager;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+}
